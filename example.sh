78ec6c8b438ffb8b343089b61e1e2f0977c5c9cd
Grégoire ALIZADEH NOIRET <gregoire.alizadehnoiret@gmail.com>
2021-11-06 15:47:17 +0100

    example.sh

--- example.sh
+++ example.sh
@@ -1,4 +1,1 @@
-# pip3 install Bio ete3 lxml docopt
-python genere_xml.py data/Ex_Arbre-SAMD9pri_MuscleCo_clado.tree data/Ex_Alignement-SAMD9pri_MuscleCo.fasta data/Ex_Resultats-SAMD9pri_M2.info || python3 genere_xml.py data/Ex_Arbre-SAMD9pri_MuscleCo_clado.tree data/Ex_Alignement-SAMD9pri_MuscleCo.fasta data/Ex_Resultats-SAMD9pri_M2.info;
-cp data/Ex_Arbre-SAMD9pri_MuscleCo_clado.tree.xml input_tree.xml;
-npm start;
+python3 genere_xml.py data/example_tree.tree data/example_alignment.fasta data/example_results.info || python genere_xml.py data/example_tree.tree data/example_alignment.fasta data/example_results.info && cp data/example_tree.tree.xml input_tree.xml && npm start
