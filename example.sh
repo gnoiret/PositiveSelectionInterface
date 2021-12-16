d48feb06968ffa8c23c9b730c40956a307e7ede2
Camille <camille.siharath@etu.univ-lyon1.fr>
2021-12-16 09:21:46 +0100

    suppression

--- example.sh
+++
@@ -1,1 +0,0 @@
-python3 genere_xml.py data/example_tree.tree data/example_alignment.fasta data/example_results.info || python genere_xml.py data/example_tree.tree data/example_alignment.fasta data/example_results.info && cp data/example_tree.tree.xml input_tree.xml && npm start
