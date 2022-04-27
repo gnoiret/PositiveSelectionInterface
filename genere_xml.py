f5f40385422ac31b8ec09db0a432cb92a149064a
Grégoire ALIZADEH NOIRET <gregoire.alizadeh-noiret@etu.univ-lyon1.fr>
2022-04-27 10:51:16 +0200

    nuc to prot function

--- genere_xml.py
+++ genere_xml.py
@@ -74,17 +74,14 @@ def nuc_acid_to_prot(dna_seq:str):
         'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
         'AGA':'R', 'AGC':'S', 'AGG':'R', 'AGT':'S',
         'ATA':'I', 'ATC':'I', 'ATG':'M', 'ATT':'I',
-
         'CAA':'Q', 'CAC':'H', 'CAG':'Q', 'CAT':'H',
         'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
         'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
         'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-
         'GAA':'E', 'GAC':'D', 'GAG':'E', 'GAT':'D',
         'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
         'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
         'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-
         'TAA':'*', 'TAC':'Y', 'TAG':'*', 'TAT':'Y',
         'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
         'TGA':'*', 'TGC':'C', 'TGG':'W', 'TGT':'C',
@@ -94,17 +91,14 @@ def nuc_acid_to_prot(dna_seq:str):
         'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACU':'T',
         'AGA':'R', 'AGC':'S', 'AGG':'R', 'AGU':'S',
         'AUA':'I', 'AUC':'I', 'AUG':'M', 'AUU':'I',
-
         'CAA':'Q', 'CAC':'H', 'CAG':'Q', 'CAU':'H',
         'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P',
         'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGU':'R',
         'CUA':'L', 'CUC':'L', 'CUG':'L', 'CUU':'L',
-
         'GAA':'E', 'GAC':'D', 'GAG':'E', 'GAU':'D',
         'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A',
         'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGU':'G',
         'GUA':'V', 'GUC':'V', 'GUG':'V', 'GUU':'V',
-
         'UAA':'*', 'UAC':'Y', 'UAG':'*', 'UAU':'Y',
         'UCA':'S', 'UCC':'S', 'UCG':'S', 'UCU':'S',
         'UGA':'*', 'UGC':'C', 'UGG':'W', 'UGU':'C',
