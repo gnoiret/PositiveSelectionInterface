b1807a7a23ef42e7e8dbb7bef1f159e352eec764
Grégoire ALIZADEH NOIRET <gregoire.alizadeh-noiret@etu.univ-lyon1.fr>
2022-05-10 14:45:09 +0200

    Temp

--- data/temp.py
+++ data/temp.py
@@ -1,5 +1,4 @@
 import re
-
 from psutil import net_if_addrs
 
 # with open('ex2_SAMD9_YN98_dNdS.count', 'r') as fi:
@@ -13,25 +12,25 @@ from psutil import net_if_addrs
 #             content += f'{line[0]}\t{omega:.8f}\n'
 #         fo.write(content)
 
-with open('ex2_SAMD9_YN98_dN.count', 'r') as fdn:
-    with open('ex2_SAMD9_YN98_dS.count', 'r') as fds:
-        with open('ex2_SAMD9_YN98_sitebranch.count', 'w+') as fo:
-            content = ''
-            n_header = fdn.readline()
-            s_header = fds.readline()
-            if n_header == s_header:
-                content += n_header
-            for line_n in fdn:
-                line_s = fds.readline()
-                line_n = line_n.strip('\n').split('\t')
-                line_s = line_s.strip('\n').split('\t')
-                print('n',line_n[0], '\ts',line_s[0])
-                print(line_s)
-                line_x = []
-                for i in range(len(line_n)):
-                    if i == 0:
-                        line_x.append(line_n[i])
-                    else:
-                        line_x.append(f'{float(line_n[i])/float(line_s[i]):.3f}')
-                content += '\t'.join(line_x)+'\n'
-            fo.write(content)
+# with open('ex2_SAMD9_YN98_dN.count', 'r') as fdn:
+#     with open('ex2_SAMD9_YN98_dS.count', 'r') as fds:
+#         with open('ex2_SAMD9_YN98_sitebranch.count', 'w+') as fo:
+#             content = ''
+#             n_header = fdn.readline()
+#             s_header = fds.readline()
+#             if n_header == s_header:
+#                 content += n_header
+#             for line_n in fdn:
+#                 line_s = fds.readline()
+#                 line_n = line_n.strip('\n').split('\t')
+#                 line_s = line_s.strip('\n').split('\t')
+#                 print('n',line_n[0], '\ts',line_s[0])
+#                 print(line_s)
+#                 line_x = []
+#                 for i in range(len(line_n)):
+#                     if i == 0:
+#                         line_x.append(line_n[i])
+#                     else:
+#                         line_x.append(f'{float(line_n[i])/float(line_s[i]):.3f}')
+#                 content += '\t'.join(line_x)+'\n'
+#             fo.write(content)
