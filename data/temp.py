ccdf5a6f6cb07350749f6ed908a67a258a39bd61
Grégoire ALIZADEH NOIRET <gregoire.alizadeh-noiret@etu.univ-lyon1.fr>
2022-05-31 13:43:00 +0200

    Manually generated dN/dS

--- data/temp.py
+++ data/temp.py
@@ -12,9 +12,9 @@ from psutil import net_if_addrs
 #             content += f'{line[0]}\t{omega:.8f}\n'
 #         fo.write(content)
 
-with open('ex2_SAMD9_YN98_dN.count', 'r') as fdn:
-    with open('ex2_SAMD9_YN98_dS.count', 'r') as fds:
-        with open('ex2_SAMD9_YN98_sitebranch.count', 'w+') as fo:
+with open('fyco1/counts_pbps_dN.count', 'r') as fdn:
+    with open('fyco1/counts_pbps_dS.count', 'r') as fds:
+        with open('fyco1/counts_pbps_dNdS.count', 'w+') as fo:
             content = ''
             n_header = fdn.readline()
             s_header = fds.readline()
@@ -31,6 +31,6 @@ with open('ex2_SAMD9_YN98_dN.count', 'r') as fdn:
                     if i == 0:
                         line_x.append(line_n[i])
                     else:
-                        line_x.append(f'{float(line_n[i])/float(line_s[i]):.8f}')
+                        line_x.append(f'{float(line_n[i])/float(line_s[i]):.3f}')
                 content += '\t'.join(line_x)+'\n'
             fo.write(content)
